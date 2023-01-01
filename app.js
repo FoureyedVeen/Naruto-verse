a2cf339f8ee9895bd054a7130ef032dc7d63139e
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-01-01 22:38:07 +0100

    cors error

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const api_url = "https://naruto-details.herokuapp.com/clan";
+// const api_url = "https://naruto-details.herokuapp.com/clan";
 
-async function getapi(url) {
+// async function getapi(url) {
 
-    const response = await fetch(url);
+//     const response = await fetch(url);
 
-    let data = await response.json();
-    data.length;
-    console.log(data);
+//     let data = await response.json();
+//     data.length;
+//     console.log(data);
+// }
+// getapi(api_url);
+async function getApi(url) {
+const response = await fetch("https://naruto-details.herokuapp.com/clan", {mode: 'no-cors'})
+    // .then(response => response.json() )
+    .then(data => {
+        console.log(data);
+        return data;
+    })
 }
-getapi(api_url);
+getApi();
