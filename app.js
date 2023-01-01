ccce3e5ff0b467e242a9aea863ccffa4e73f8095
“FoureyedVeen” <cosmasdaniella@gmail.com>
2023-01-01 20:35:24 +0100

    first try

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const api_url = "https://naruto-details.herokuapp.com/clan";
 
 async function getapi(url) {
 
-    const reply = await fetch(url);
+    const response = await fetch(url);
 
-    let data = await reply.json();
+    let data = await response.json();
     data.length;
     console.log(data);
 }
